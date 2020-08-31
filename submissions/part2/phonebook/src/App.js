a7833ff2e88c821faf2ca6a8074c2379ed3ade6e
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2020-08-31 11:52:00 -0700

    Part 2, exercise 2.9

--- submissions/part2/phonebook/src/App.js
+++ submissions/part2/phonebook/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const allnames = persons.map(p => p.name)
   const [ newName, setNewName ] = useState('')
   const [ newNum, setNewNum ] = useState('') 
+  const [ search, setSearch ] = useState('')
 
   const addPerson = (event) => {
 
@@ -21,6 +25,7 @@ const App = () => {
       }
       setPersons(persons.concat(personobject))
       setNewName('')
+      setNewNum('')
     }
   }
 
@@ -34,9 +39,23 @@ const App = () => {
     setNewNum(event.target.value)
   }
 
+
+  const updateSearch = (event) => {
+    console.log(event.target.value)
+    setSearch(event.target.value)
+  }
+  
+  const filteredPersons = persons.filter((p) => p.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <input
+      value={search}
+      onChange={updateSearch}
+      />
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -53,7 +72,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{persons.map(p => <li key={p.name}><strong>{p.name}</strong> {p.number}</li>)}</div>
+      <div>{filteredPersons.map(p => <li key={p.name}><strong>{p.name}</strong> {p.number}</li>)}</div>
     </div>
   )
 }
