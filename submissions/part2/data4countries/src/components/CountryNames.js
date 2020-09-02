c15fe21056bef729c0d8096e047eb2e62d001169
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2020-09-02 09:38:48 -0700

    all changes to complete Part 2, exercise 2.13 (Data for countries)

--- submissions/part2/data4countries/src/components/CountryNames.js
+++ submissions/part2/data4countries/src/components/CountryNames.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FullData from './FullData'
 
-const CountryNames = ({fc}) => {
+const CountryNames = ({fc, ac}) => {
+    const [ fullCountry, setFullCountry ] = useState([])
+    const [ showFull, setShowFull ] = useState(false)
 
-    console.log(fc)
-    return (<>{fc.map(c => 
-        <li key={c}>{c}</li>)}
-        </>
-        )
+   
+    const changeState = ({c}) => {
+        setFullCountry([c])
+        setShowFull(true)
     }
+
+    if (showFull) {
+        return (<FullData fc={fullCountry} ac={ac}/>)
+    }
+
+    else {
+        return (<>{fc.map(c => <>
+            <li key={c}>{c}<button onClick={() => changeState({c})}>show {c}</button></li></>)}
+            </>
+            )
+    }
+}
+
 
 export default CountryNames
