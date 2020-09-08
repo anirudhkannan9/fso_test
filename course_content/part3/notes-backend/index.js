d912f7fd3b1b00d0232cb74c0b7afd85732634eb
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2020-09-08 15:25:52 -0700

    Changes for Part 3a (course content, not submissions). Implementing Express backend for notes app.

--- course_content/part3/notes-backend/index.js
+++ course_content/part3/notes-backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-
 app.use(express.json())
+const cors = require('cors')
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -87,8 +88,6 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
-
-
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
