4c7e51a2263ad27f24aca8c38fc00d18208271ae
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2020-09-11 16:15:29 -0700

    Changes for Part3b of course content: Deploying app to Internet -> Serving static files from backend. Created production build of frontend React app, copied production build to root of backend. Backend now serves a static file that acts as frontend, getting data from backend. Next step: deploy to Heroku.

---
+++ course_content/part3/notes-backend/build/precache-manifest.d7f8cfa899ec72183ec4cbbc8a5f11cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c198ad74f301d4f69657bcee253a8467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ace5367a6d7d53d8a5cf",
+    "url": "/static/css/main.3f41024e.chunk.css"
+  },
+  {
+    "revision": "440664b0771dc5d97d65",
+    "url": "/static/js/2.72393813.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72393813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace5367a6d7d53d8a5cf",
+    "url": "/static/js/main.2d43b860.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
---
+++ course_content/part3/notes-backend/build/precache-manifest.d957bcaa9c58f4e30d916b420fdb55cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6e3f8a20e86984e242c170f894bf2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7124267e2b176e3dd7d7",
+    "url": "/static/css/main.3f41024e.chunk.css"
+  },
+  {
+    "revision": "440664b0771dc5d97d65",
+    "url": "/static/js/2.72393813.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72393813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7124267e2b176e3dd7d7",
+    "url": "/static/js/main.4feaf9fe.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
