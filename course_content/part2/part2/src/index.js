245be9602c4a6bf2afdffaea046ad3fbd718de8f
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2020-09-12 21:59:01 -0700

    Changes for Part 3; course content (prior to exercises); creating a backend server w/ express to host notes and then adding frontend to that. Hosting on Heroku.

--- course_content/part2/part2/src/index.js
+++ course_content/part2/part2/src/index.js
@@ -5,7 +5,7 @@ import App from './components/App'
 import axios from 'axios'
 
 axios
-.get('http://localhost:3001/api/notes')
+.get('/api/notes')
 .then(response => {
   ReactDOM.render(<App/>, document.getElementById('root'))
 })
---
+++ course_content/part3/notes-backend/build/precache-manifest.756e00a6a20e637c702207da5423709f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49932e7444d9e3e94ffd467aac4f671",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce7394edfb730fe99806",
+    "url": "/static/css/main.3f41024e.chunk.css"
+  },
+  {
+    "revision": "440664b0771dc5d97d65",
+    "url": "/static/js/2.72393813.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72393813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7394edfb730fe99806",
+    "url": "/static/js/main.a5ef040c.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
